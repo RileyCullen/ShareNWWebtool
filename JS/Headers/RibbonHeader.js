2270ab5cdac46c18d16c2d96e801ee8294b6c63c
Riley Cullen <cullenriley@seattleu.edu>
2020-11-16 10:45:34 -1000

    Documentation Updated.

--- JS/Headers/RibbonHeader.js
+++ JS/Headers/RibbonHeader.js
@@ -1,5 +1,23 @@
+// Cullen, Riley
+// RibbonHeader.js
+// November 16, 2020
+
 class RibbonHeader 
 {
+    /**
+     * @summary     Creates a ribbon like icon.
+     * @description Creates a ribbon like icon by creating three rounded rectangles.
+     * 
+     * @requires Konva.js
+     * 
+     * @param {String}      colorOne The color of the large, central rectangle.
+     * @param {String}      colorTwo The color of the two smaller rectangles.
+     * @param {Konva.Group} group    The group we add the ribbon to.
+     * @param {Double}      hWidth   The width of the ribbon.
+     * @param {Double}      hHeight  The height of the ribbon.
+     * @param {Double}      iWidth   The width of the infographic.
+     * @param {Double}      iHeight  The height of the infographic.
+     */
     constructor(colorOne, colorTwo, group, hWidth, hHeight, iWidth, iHeight)
     {
         this._colorOne = colorOne;
@@ -11,12 +29,20 @@ class RibbonHeader
         this._infogHeight = iHeight;
     }
 
+    /**
+     * @summary Creates the ribbon.
+     */
     CreateHeader()
     {
         this._CreateMainRect();
         this._CreateSideRects();
     }
 
+    /**
+     * @summary     Creates the main, central rectangle in the ribbon.
+     * @description Adds a Konva.Rect to the group. The position of the Rect is
+     *              dynamically calculated based on the width of the ribbon.
+     */
     _CreateMainRect()
     {
         this._group.add(new Konva.Rect({
@@ -29,6 +55,13 @@ class RibbonHeader
         }));
     }
 
+    /**
+     * @summary     Creates the two smaller side rectangles.
+     * @description Dynamically calculates the x positions of the two smaller 
+     *              rectangles so that they are roughly half way down. These
+     *              values are then passed to _CreateMiniRect() which actually 
+     *              creates the Konva.Rect's.
+     */
     _CreateSideRects()
     {
         var leftX = ((this._infogWidth / 2) - (this._width / 2)) - ((3/4) * this._height / 2),
@@ -37,6 +70,14 @@ class RibbonHeader
         this._CreateMiniRect(rightX); 
     }
 
+    /**
+     * @summary     Creates a smaller rectangles to the side of the main rectangle.
+     * @description Adds a Konva.Rect's to the group. This is done by dynamically
+     *              calculating the x position so that it is roughly one half of the
+     *              way down.
+     * 
+     * @param {Double} x The x position of the rectangle.
+     */
     _CreateMiniRect(x)
     {
         var tmp = new Konva.Rect({
