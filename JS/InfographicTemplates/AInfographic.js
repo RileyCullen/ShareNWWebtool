7575974a9a6e61e1f723a0e6c2011435c3a7782c
Riley Cullen <cullenriley@seattleu.edu>
2020-11-28 19:25:35 -1000

    Support for html2canvas added.

--- JS/InfographicTemplates/AInfographic.js
+++ JS/InfographicTemplates/AInfographic.js
@@ -109,139 +109,41 @@ class AInfographic
         return center - (width / 2);
     }
 
+    _FinalizeInfog()
+    {
+        this._RenderText();
+        this._AddGraphSelection();
+        this._AddTextSelection();
+    }
+
+    _RenderText()
+    {
+        var helperElem = document.createElement('div');
+        helperElem.style.position = 'absolute';
+        document.getElementById('body').appendChild(helperElem);
+
+        for (var i = 0; i < this._textHandler.GetSize(); i++) {
+            helperElem.appendChild(this._textHandler.GetTextElem(i));
+            this._HTMLToCanvas(i);
+            this._textHandler.GetTextElem(i).remove();
+        }
+        helperElem.remove();
+    }
+
+    _HTMLToCanvas(index)
+    {
+        console.log(document.querySelector('.EditableText'));
+        html2canvas(document.querySelector('.EditableText'), {
+            backgroundColor: null,
+        }).then((image) => {
+            this._textHandler.GetImage(index).image(image);
+            this._main.batchDraw();
+        });
+    }
+
     _AddTextSelection()
     {
-        var selection = this._stage.find('.EditableText');
-        var chartWidth = this._chartWidth;
-        selection.each(textNode => {
-            textNode.on('dblclick', () => {
-                var HandleOutsideClick = (e) => {
-                    if (e.target !== textNode) {
-                        textNode.text(textarea.value);
-                        RemoveTextArea();
-                        this._main.batchDraw();
-                        this._stage.off('click', HandleOutsideClick);
-                    }
-                };
-
-                var RemoveTextArea = () => {
-                    textarea.parentNode.removeChild(textarea);
-                    window.removeEventListener('click', HandleOutsideClick);
-                    textNode.show();
-                    this._tr.show();
-                    this._tr.forceUpdate();
-                    this._main.draw();
-                };
-
-                function _GetTextWidth(text, fontSize, fontFamily)
-                {
-                    var canvas = document.createElement('canvas');
-                    var ctx = canvas.getContext('2d');
-
-                    ctx.font = fontSize + 'px ' + fontFamily;
-                    var helper = ctx.measureText(text).width;
-                    canvas.remove();
-
-                    return helper;
-                }
-
-                function _CenterXAbout(width, center)
-                {
-                    return center - (width / 2);
-                }
-
-                textNode.hide();
-                this._tr.hide();
-                this._main.draw();
-
-                var textPosition = textNode.absolutePosition();
-
-                // then lets find position of stage container on the page:
-                var stageBox = this._stage.container().getBoundingClientRect();
-
-                // so position of textarea will be the sum of positions above:
-                var areaPosition = {
-                    x: stageBox.left + textPosition.x,
-                    y: stageBox.top + textPosition.y,
-                };
-
-                // create textarea and style it
-                var textarea = document.createElement('textarea');
-                document.body.appendChild(textarea);
-
-                // css that tries to put the textarea where it is on the canvas
-                textarea.value = textNode.text();
-                textarea.style.position = 'absolute';
-                textarea.style.top = areaPosition.y + 'px';
-                textarea.style.left = areaPosition.x + 'px';
-                textarea.style.width = textNode.width() - textNode.padding() * 2 + 'px';
-                textarea.style.height = textNode.height() - textNode.padding() * 2 + 5 + 'px';
-                textarea.style.fontSize = textNode.fontSize() + 'px';
-                textarea.style.borderStyle = 'solid';
-                textarea.style.padding = '0px';
-                textarea.style.margin = '0px';
-                textarea.style.overflow = 'hidden';
-                textarea.style.background = 'none';
-                textarea.style.outline = 'none';
-                textarea.style.resize = 'both';
-                textarea.style.lineHeight = textNode.lineHeight();
-                textarea.style.fontFamily = textNode.fontFamily();
-                textarea.style.transformOrigin = 'left top';
-                textarea.style.textAlign = textNode.align();
-                textarea.style.color = textNode.fill();
-                var rotation = textNode.rotation();
-                var transform = '';
-                if (rotation) {
-                    transform += 'rotateZ(' + rotation + 'deg)';
-                }
-
-                var px = 0;
-                // also we need to slightly move textarea on firefox
-                // because it jumps a bit
-                var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-                if (isFirefox) {
-                    px += 2 + Math.round(textNode.fontSize() / 20);
-                }
-                transform += 'translateY(-' + px + 'px)';
-
-                textarea.style.transform = transform;
-
-                // reset height
-                textarea.style.height = 'auto';
-                // after browsers resized it we can set actual value
-                textarea.style.height = textarea.scrollHeight + 3 + 'px';
-
-                textarea.focus();
-
-                textarea.addEventListener('keydown', function (e) {
-                    // hide on enter
-                    // but don't hide on shift + enter
-                    if (e.keyCode === 13 && !e.shiftKey) {
-                        textNode.text(textarea.value);
-                        var width = _GetTextWidth(textarea.value, textNode.getAttr('fontSize'), textNode.getAttr('fontFamily'));
-                        var newX = _CenterXAbout(width, chartWidth / 2);
-                        textNode.x(newX);
-                        RemoveTextArea();
-                    }
-                    // on esc do not set value back to node
-                    if (e.keyCode === 27) {
-                        RemoveTextArea();
-                    }
-                });
-
-                textarea.addEventListener('keydown', function (e) {
-                    var scale = textNode.getAbsoluteScale().x;
-                    //SetTextAreaWidth(textNode.width() * scale);
-                    textarea.style.height = 'auto';
-                    textarea.style.height = textarea.scrollHeight + textNode.fontSize() + 'px';
-                });
-
-                setTimeout(() => {
-                    this._stage.on('click', HandleOutsideClick);
-                });
-
-            });
-        });
+        
     }
 
     _AddGraphSelection()
