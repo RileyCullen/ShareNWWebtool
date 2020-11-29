37abbd918bba9ddc554adc9041c7ffe8840ba6e7
Riley Cullen <cullenriley@seattleu.edu>
2020-11-28 19:27:59 -1000

    Support for html2canvas text elements added.

--- JS/InfographicTemplates/Handlers/TextHandler.js
+++ JS/InfographicTemplates/Handlers/TextHandler.js
@@ -6,19 +6,30 @@ class TextHandler
         this._curr = -1;
     }
 
-    AddTextElem(textElem, group)
+    AddTextElem(textElem, group, x = 0, y = 0)
     {
         this._curr++;
         this._handler[this._curr] = {
             'textElem': textElem,
-            'group': group
+            'group': group,
+            'image': new Konva.Image({
+                draggable: true,
+                scaleX: 1 / window.devicePixelRatio,
+                scaleY: 1 / window.devicePixelRatio,
+                x: x, 
+                y: y,
+            }),
         };
-        group.add(textElem);
+        textElem.id = this._curr;
+        textElem.className = 'EditableText';
+        textElem.style.textAlign = 'left';
+        group.add(this._handler[this._curr].image);
         group.setAttr('id', this._curr);
-        textElem.setAttr('name', 'EditableText');
     }
 
-    GetCurrID() { return this._curr; }
+    GetCurrID()     { return this._curr; }
+    GetSize()       { return this._curr + 1; }
     GetTextElem(id) { return this._handler[id].textElem; }
-    GetGroup(id) { return this._handler[id].group; } 
+    GetGroup(id)    { return this._handler[id].group; } 
+    GetImage(id)    { return this._handler[id].image; }
 }
