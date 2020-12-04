20df5046dfe356adbd28069f98782f4d3a78ebab
Riley Cullen <cullenriley@seattleu.edu>
2020-12-03 15:36:23 -1000

    Documentation added.

--- JS/InfographicTemplates/Handlers/TextHandler.js
+++ JS/InfographicTemplates/Handlers/TextHandler.js
@@ -1,11 +1,34 @@
+// Cullen, Riley
+// TextHandler.js
+// December 3, 2020
+
 class TextHandler
 {
+    /**
+     * @summary     A concrete class that manages all of the text elements within
+     *              an infographic.
+     * @description Groups all of the individual text elements into one place 
+     *              so that operations on text are easier to perform.
+     */
     constructor()
     {
         this._handler = [];
         this._curr = -1;
     }
 
+    /**
+     * @summary     Adds a text element to the handler.
+     * @description Adds a handler element at this._curr to the handler. This 
+     *              element includes: the text to be rendered, its associated 
+     *              group, and the Konva.Image element that corresponds to the 
+     *              text.
+     * 
+     * @param {HTML Element} textElem The text we wish to render.
+     * @param {Konva.Group}  group    The group we want to add the rendered text
+     *                                to.
+     * @param {double}       x        The x position of the image within the group.
+     * @param {double}       y        The y position of the image within the group.
+     */
     AddTextElem(textElem, group, x = 0, y = 0)
     {
         this._curr++;
@@ -13,7 +36,6 @@ class TextHandler
             'textElem': textElem,
             'group': group,
             'image': new Konva.Image({
-                draggable: true,
                 scaleX: 1 / window.devicePixelRatio,
                 scaleY: 1 / window.devicePixelRatio,
                 x: x, 
@@ -28,9 +50,31 @@ class TextHandler
         group.setAttr('id', this._curr);
     }
 
+    /**
+     * @summary Accessor that returns the current index of the handler.
+     */
     GetCurrID()     { return this._curr; }
+
+    /**
+     * @summary Accessor that returns the current size of the handler.
+     */
     GetSize()       { return this._curr + 1; }
+
+    /**
+     * @summary Accessor that returns the text element at index id.
+     * @param {int} id The index of the text element we want to find. 
+     */
     GetTextElem(id) { return this._handler[id].textElem; }
+
+    /**
+     * @summary Accessor that returns the Konva.Group element at index id.
+     * @param {int} id The index of the text element we want to find. 
+     */
     GetGroup(id)    { return this._handler[id].group; } 
+
+    /**
+     * @summary Accessor that returns the Konva.Image element at index id.
+     * @param {int} id The index of the text element we want to find. 
+     */
     GetImage(id)    { return this._handler[id].image; }
 }
