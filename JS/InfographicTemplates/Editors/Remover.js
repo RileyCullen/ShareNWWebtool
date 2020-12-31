e27b0d5ebc114d91574deaa0b430bc7c487262bd
Riley Cullen <cullenriley@seattleu.edu>
2020-12-30 15:05:55 -1000

    Documentation added.

--- JS/InfographicTemplates/Editors/Remover.js
+++ JS/InfographicTemplates/Editors/Remover.js
@@ -1,5 +1,15 @@
+// Cullen, Riley
+// Remover.js
+// December 30, 2020
+
 class Remover
 {
+    /**
+     * @summary     Removes infographic elements from canvas when selected.
+     * @description Creates an object that handles the removal of selectable 
+     *              infographic elements from the canvas. Also creates the UI 
+     *              elements responsible for removal.
+     */
     constructor()
     {
        this._elem = null;
@@ -7,6 +17,11 @@ class Remover
        this._layer = null;
     }
 
+    /**
+     * @summary     Creates the UI responsible for removing selected infographic elements.
+     * @description Creates a <div> element with id "Remover" and returns it to the 
+     *              caller. This <div> element contains a button.
+     */
     CreateRemoverUI()
     {
         var container = document.createElement('div');
@@ -15,6 +30,16 @@ class Remover
         return container;
     }
 
+    /**
+     * @summary     Sets _elem, which is the element we want to remove from the 
+     *              canvas.
+     * @description Assigns textElem to _elem, sets the type to "text", and sets
+     *              _layer to layer so we can call batchDraw. This function also
+     *              updates the button listener.
+     * 
+     * @param {Konva.Image} textElem The text element we want to remove.
+     * @param {Konva.Layer} layer    The main layer of the infographic.
+     */
     SetTextElem(textElem, layer)
     {
         this._elem = textElem;
@@ -23,6 +48,14 @@ class Remover
         this._UpdateButtonListener();
     }
 
+    /**
+     * @summary     Sets _elem to chartHandler and _type to 'chart'.
+     * @description Sets up instance variables (see SetTextElem description).
+     * 
+     * @param {JSONArray} chartHandler The chart handler element associated with
+     *                                 the chart we want to remove.
+     * @param {Konva.Layer} layer      The main layer of the canvas.
+     */
     SetChartElem(chartHandler, layer)
     {
         this._elem = chartHandler;
@@ -31,6 +64,11 @@ class Remover
         this._UpdateButtonListener();
     }
 
+    /**
+     * @summary     Resets the current Remover object.
+     * @description Nullifies all of the instance variables so infographic elements
+     *              aren't deleted when unselected.
+     */
     ResetRemover()
     {
         this._elem = null;
@@ -38,6 +76,13 @@ class Remover
         this._layer = null;
     }
 
+    /**
+     * @summary     Creates a button responsible for removing selected elements.
+     * @description Creates a button with id "RemoverButton" and a trash icon. 
+     *              This button is responsible for removing selected infographic 
+     *              elements but this capability is not set here and is instead 
+     *              set in the SetChart and SetText methods.
+     */
     _CreateButton()
     {
         var container = document.createElement('div');
@@ -52,6 +97,13 @@ class Remover
         return container;
     }
 
+    /**
+     * @summary     Event listener responsible for removing selected infographic 
+     *              element.
+     * @description Event listener bound to the button created in _CreateButton.
+     *              This function determines the type of _elem and then calls
+     *              its type's respective removal funcion.
+     */
     _RemoveObject()
     {
         if (this._elem) {
@@ -63,18 +115,32 @@ class Remover
         }
     }
 
+    /**
+     * @summary     Removes text element from infographic.
+     * @description Calls _elem's destroy function and then redraws the changed
+     *              portion of the infographic.
+     */
     _RemoveTextElem()
     {
         this._elem.destroy();
         this._layer.batchDraw();
     }
 
+    /**
+     * @summary     Removes chart from infographic.
+     * @description Calls _elem.chart's Remove function and redraws the changed
+     *              portion of the canvas.
+     */
     _RemoveChart()
     {
         this._elem.chart.Remove();
         this._layer.batchDraw();
     }
 
+    /**
+     * @summary     Updates the button's event listener.
+     * @description Set's the button's event listener to _RemoveObject.
+     */
     _UpdateButtonListener()
     {
         var button = document.querySelector('#RemoverButton');
