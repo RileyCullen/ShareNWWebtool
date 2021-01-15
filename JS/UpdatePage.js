7399e7bc68e895f38f58fcb985d91e6368978c3a
Riley Cullen <cullenriley@seattleu.edu>
2021-01-14 21:23:19 -1000

    Placeholder text added.

--- JS/UpdatePage.js
+++ JS/UpdatePage.js
@@ -2,6 +2,10 @@ var infogObj;
 function UpdatePage(infogNum)
 {
     if (infogObj !== undefined) infogObj.Remove();
+
+    console.log(document.getElementById('Placeholder'));
+    if (document.getElementById('Placeholder') !== null) RemovePlaceholder();
+
     switch(infogNum) {
         case 0: 
             infogObj = new HIVTemplateOne();
@@ -14,4 +18,19 @@ function UpdatePage(infogNum)
     }
     infogObj.CreateInfographic();
     infogObj.Draw();
+    CreatePlaceholder();
+}
+
+function CreatePlaceholder()
+{
+    var editor = document.getElementById('editor');
+    var message = document.createElement('p');
+    message.id = 'Placeholder';
+    message.innerHTML = '<-- Double click on a chart or text element to begin editing!';
+    editor.appendChild(message);
+}
+
+function RemovePlaceholder()
+{
+    document.getElementById('Placeholder').remove();
 }
