9ef4f02adf711fa6b03d4c64fc7869a95ebd6e15
Riley Cullen <cullenriley@seattleu.edu>
2021-06-29 12:06:37 -1000

    Support for NavBar added.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,49 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {InfographicEditor} from './components/InfographicEditor/InfographicEditor';
+import { InfographicEditor } from './components/InfographicEditor/InfographicEditor';
+import { NavigationBar } from './components/NavigationBar/NavigationBar';
+
+import './css/React/NavigationBar.css';
 
 /**
  * The "head" react class for our application. 
  */
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            currentInfographic: 'HIVTemplateOne'
+        }
+    }
+
     render()
     {
+        var currentInfographic = this.state.currentInfographic;
         return (
-            <InfographicEditor />
+            <div className='AppContainer'>
+                <NavigationBar setInfographic={(infog) => { this._SetCurrentInfographic(infog); }}/>
+                <div className='tabcontent'>
+                    <InfographicEditor currentInfographic={currentInfographic}/>
+                </div>
+            </div>
         );
+    }
+
+    /**
+     * @summary     Sets currentInfographic to a new infographic.
+     * @description Calls setState and passes assigns infographic to 
+     *              currentInfographic to initiate rendering process.
+     * 
+     * @param {String} infographic The name of the infographic we want to display.
+     */
+    _SetCurrentInfographic(infographic)
+    {
+        this.setState({
+            currentInfographic: infographic,
+        });
     }
 }
 
