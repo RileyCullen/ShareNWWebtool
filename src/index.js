05f9d33d428d62a71814cf325a54a863e13d4be0
Riley Cullen <cullenriley@seattleu.edu>
2021-06-28 14:36:53 -1000

    Render updated to return an InfographicEditor object.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {InfographicEditor} from './Components/InfographicEditor/InfographicEditor';
 
 class App extends React.Component
 {
     render()
     {
         return (
-            <p>hello</p>
+            <InfographicEditor />
         );
     }
 }
