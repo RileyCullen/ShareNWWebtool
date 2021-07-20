70573628dd639de5558b48c9c1cd562ac3087da4
Riley Cullen <cullenriley@seattleu.edu>
2021-07-19 14:20:57 -1000

    Initial commit.

---
+++ src/js/Headers/RectangleHeader.js
@@ -0,0 +1,30 @@
+import Konva from 'konva';
+
+class RectangleHeader
+{
+    constructor({x, y, width, height, cornerRadius, fill, group})
+    {
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+        this._cornerRadius = cornerRadius;
+        this._fill = fill;
+        this._group = group;
+    }
+
+    CreateHeader()
+    {
+        var rect = new Konva.Rect({
+            cornerRadius: this._cornerRadius, 
+            x: this._x, 
+            y: this._y,
+            width: this._width,
+            height: this._height,
+            fill: this._fill,
+        });
+        this._group.add(rect); 
+    }
+}
+
+export { RectangleHeader };
