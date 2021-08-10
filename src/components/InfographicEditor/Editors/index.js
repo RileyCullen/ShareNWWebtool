2bc0cf917192cffd0071cc31d4ffce2ba8a9e45d
Riley Cullen <cullenriley@seattleu.edu>
2021-08-09 23:07:48 -1000

    Support for new editor types added.

--- src/components/InfographicEditor/Editors/index.js
+++ src/components/InfographicEditor/Editors/index.js
@@ -2,3 +2,7 @@ export { QuillEditor } from './QuillEditor';
 export { WaffleEditor } from './WaffleEditor';
 export { BarEditor } from './BarEditor';
 export { Remover } from './Remover';
+export { IconBarEditor } from './IconBarEditor';
+export { PieEditor } from './PieEditor';
+export { StackedBarEditor } from './StackedBarEditor';
+export { LineEditor } from './LineEditor';
