ac2ccff112cfa3bb5c7d5a5fa64d366e758b312e
Riley Cullen <cullenriley@seattleu.edu>
2021-08-10 16:05:13 -1000

    Updated to support new editor components.

--- src/components/InfographicEditor/Editors/Components/index.js
+++ src/components/InfographicEditor/Editors/Components/index.js
@@ -1,3 +1,6 @@
 export { TextField } from './TextField';
 export { Tab } from './Tab';
 export { TabContainer } from './TabContainer';
+export { Menu } from './Menu';
+export { Editor } from './Editor';
+export { Checkbox } from './Checkbox';
