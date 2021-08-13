df30eed29d1250858f78f7575ed2ff3fe363c5ef
Riley Cullen <cullenriley@seattleu.edu>
2021-08-12 23:08:12 -1000

    initial commit.

---
+++ src/components/InfographicEditor/Editors/Components/LabeledColorPicker.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { ColorPicker } from './ColorPicker';
+
+import '../../../../css/React/Editors/LabeledColorPicker.css';
+
+class LabeledColorPicker extends React.Component 
+{
+    render() 
+    {
+        return (
+            <div className='labeled-color-picker-container'>
+                <label>{this.props.label}</label>
+                <div className='labeled-color-picker'>
+                    <ColorPicker 
+                        color={this.props.color}
+                        onChange={(value) => { this.props.onChange(value); }}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export { LabeledColorPicker };
