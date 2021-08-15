cf18fa28097808ba69844f0229436f8d42260e82
Riley Cullen <cullenriley@seattleu.edu>
2021-08-14 22:52:12 -1000

    Initial commit.

---
+++ src/js/Charts/PieChart/ConcreteCharts/DonutChart.js
@@ -0,0 +1,37 @@
+import { APieChart } from '../APieChart';
+import { PieChart, DonutDecorator } from '../index';
+
+class DonutChart extends APieChart
+{
+    constructor({data, group, radius = 100, innerRadius = 25, innerColor='#fff'})
+    {
+        super(data, group, radius);
+        this._innerRadius = innerRadius;
+        this._innerColor = innerColor;
+    }
+
+    CreateChart()
+    {
+        let chart = new PieChart({
+            data: this._data, 
+            group: this._group, 
+            radius: this._radius
+        }),
+        decorator = new DonutDecorator({
+            chart: chart,
+            innerRadius: this._innerRadius,
+            color: this._innerColor
+        });
+        decorator.CreateChart();
+    }
+
+    GetChartSettings()
+    {
+        return {
+            size: { chartRadius: this._radius, innerRadius: this._innerRadius},
+            color: { innerColor: this._innerColor }
+        }
+    }
+}
+
+export { DonutChart };
