db7d9eff6dd5100a45d8be8dea61b458975f9367
Riley Cullen <cullenriley@seattleu.edu>
2021-08-18 15:13:06 -1000

    Data array for last pie chart fixed.

--- src/js/InfographicTemplates/ViolenceTemplateOne.js
+++ src/js/InfographicTemplates/ViolenceTemplateOne.js
@@ -579,7 +579,8 @@ class ViolenceTemplateOne extends AInfographic
         waffleChartGroup.add(circleGroup);
 
         var circle = new PieChart({
-            data: [{'category': 'test', 'value': 74, 'color': yellow}], 
+            data: [{'category': 'test', 'value': 74, 'color': yellow},
+                   {category: 'test2', value: 26, color: yellow}], 
             group: circleGroup, 
             radius: 55
         });
