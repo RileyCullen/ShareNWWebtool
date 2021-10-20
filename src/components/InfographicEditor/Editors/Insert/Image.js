a2257acc25f4b5835d2b8afdb421c5d0ac7f1df2
mlee2328 <79023012+mlee2328@users.noreply.github.com>
2021-10-19 20:15:53 -0700

    Worked on uploading images
    Images can be inserted into the infographic, but they aren't displayed.

--- src/components/InfographicEditor/Editors/Insert/Image.js
+++ src/components/InfographicEditor/Editors/Insert/Image.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import '../../../../css/React/Editors/Insert/Insert.css';
-import '../../../../css/React/Editors/Insert/Image.css'
+import '../../../../css/React/Editors/Insert/Image.css';
 
 class Image extends React.Component 
 {
@@ -10,12 +10,17 @@ class Image extends React.Component
         return (
             <div className='editor-insert-container'>
                 <div id='upper-image-upload-container'>
-                    <button id='upload-image-button'>
-                        Upload
-                    </button>
+                    <input id="upload-image-input" type="file" accept="image/png, image/jpeg" onChange={function() {
+                        if (document.getElementById("upload-image-input").value){
+                            var fileinput = document.getElementById("upload-image-input").files[0].name;
+                            var image = document.createElement("img");
+                            image.src = fileinput;
+                            document.getElementsByClassName("konvajs-content")[0].appendChild(image);
+                        }
+                    }}></input>
                 </div>
                 <div>
-                    <p id='image-placeholder' className='editor-placeholder-text'>Click upload to add an image to the library!</p>
+                    <div id='image-placeholder' className='editor-placeholder-text'>Click upload to add an image to the library!</div>
                 </div>
             </div>
         );
