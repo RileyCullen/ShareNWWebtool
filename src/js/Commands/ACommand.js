9a25e183072822603eb635ce428e0ee5482eef29
Riley Cullen <cullenriley@seattleu.edu>
2021-10-26 22:49:29 -0700

    constuctor implemented

--- src/js/Commands/ACommand.js
+++ src/js/Commands/ACommand.js
@@ -4,7 +4,23 @@
 
 class ACommand 
 {
+    constructor()
+    {
+        // if ACommand is instantiated, throw an error
+        if (ACommand === this.constructor) {
+            throw new TypeError('Abstract class "ACommand" cannot be instantiated');
+        }
 
+        // if a class extending ACommand does not have Execute or Unexecute
+        // defined, throw an error
+        if (this.Execute === undefined) {
+            throw new TypeError('Types extending "ACommand" must implement Execute');
+        }
+
+        if (this.Unexecute === undefined) {
+            throw new TypeError('Types extending "ACommand" must implement Unexecute');
+        }
+    }
 }
 
 export { ACommand };
