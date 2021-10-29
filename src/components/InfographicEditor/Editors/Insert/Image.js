800c45fb4f63874bd716827d5a0a693b687c4816
mlee2328 <79023012+mlee2328@users.noreply.github.com>
2021-10-28 17:33:58 -0700

    took out comments

--- src/components/InfographicEditor/Editors/Insert/Image.js
+++ src/components/InfographicEditor/Editors/Insert/Image.js
@@ -13,24 +13,8 @@ class Image extends React.Component
             <div className='editor-insert-container'>
                 <div id='upper-image-upload-container'>
                     <input id="upload-image-input" type="file" accept="image/png, image/jpeg, image/jpg" onChange={(event) => {
-                        if (document.getElementById("upload-image-input").value){
+                        if (document.getElementById("upload-image-input").value)
                             this.props.toggleInsert('image', URL.createObjectURL(event.target.files[0]));
-
-                            //var image = document.createElement("img");
-                            //var image = new Konva.Image();
-                            //image.src = URL.createObjectURL(event.target.files[0]);
-                            //document.getElementsByClassName("konvajs-content")[0].appendChild(image);
-                            //document.getElementById("image-placeholder").innerHTML = "";
-                            //document.getElementById("image-placeholder").appendChild(image);
-
-                            //InsertElement('image', image.src);
-
-                            //document.querySelector(".konvajs-content canvas").getContext("2d").drawImage(image, 0, 0, 150, 150);
-                            //document.getElementsById("canvas").getContext("2d").drawImage(image,0,0);
-
-                            //this.InsertElement('image',image.src);
-                            //this.props.toggleInsert('image', image.src);
-                        }
                     }}></input>
                 </div>
                 <div id='image-placeholder' className='editor-placeholder-text'>Click upload to add an image to the library!</div>
