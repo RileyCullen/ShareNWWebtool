ad40a4bb036173e5b8f7d06b0562bca52a3f793a
Riley Cullen <cullenriley@seattleu.edu>
2021-10-30 16:18:28 -0700

    Execute/Unexecute methods updated to call _layerCommand's Execute function

--- src/js/Commands/CanvasCommands/PositionCommand.js
+++ src/js/Commands/CanvasCommands/PositionCommand.js
@@ -2,11 +2,12 @@
 // ACommand.js
 // October 27, 2021
 
+import { AutoLayerCommand } from "../index";
 import { ACommand } from "../ACommand";
 
 class PositionCommand extends ACommand
 {
-    constructor({element, x, y})
+    constructor({element, x, y, z, containers})
     {
         super();
         this._element = element;
@@ -14,16 +15,23 @@ class PositionCommand extends ACommand
         this._y0 = y; // starting y
         this._x1 = x; // ending (current) x
         this._y1 = y; // ending (current) y
+        this._z0 = z;
+        this._z1 = z;
+        this._layerCommand = new AutoLayerCommand({
+            containers: containers,
+            element: this._element
+        });
     }
 
     /**
      * @summary Sets the current coordinates.
      * @param {*} param0 
      */
-    SetCurrentCoordinates({x ,y}) 
+    SetCurrentCoordinates({x, y, z}) 
     { 
         this._x1 = x;
         this._y1 = y;
+        this._z1 = z;
     }
      
     /**
@@ -35,6 +43,8 @@ class PositionCommand extends ACommand
             x: this._x1,
             y: this._y1,
         });
+        this._layerCommand.Execute();
+        this._element.setZIndex(this._z1);
     }
 
     /**
@@ -46,6 +56,8 @@ class PositionCommand extends ACommand
             x: this._x0,
             y: this._y0,
         });
+        this._layerCommand.Execute();
+        this._element.setZIndex(this._z0);
     }
 }
 
