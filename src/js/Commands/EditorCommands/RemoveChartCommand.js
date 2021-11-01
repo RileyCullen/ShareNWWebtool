fd0db85b322cdd7173ef325e383d62dbb7a59842
Riley Cullen <cullenriley@seattleu.edu>
2021-11-01 14:47:30 -0700

    constructor, Execute, and Unexecute defined and implemented

--- src/js/Commands/EditorCommands/RemoveChartCommand.js
+++ src/js/Commands/EditorCommands/RemoveChartCommand.js
@@ -2,7 +2,42 @@ import { ACommand } from '../ACommand';
 
 class RemoveChartCommand extends ACommand 
 {
-    
+    constructor({id, handler, transformer, main})
+    {
+        super();
+        this._id = id;
+        this._handler = handler;
+        this._tr = transformer;
+        this._main = main;
+
+        this._chart = this._handler.GetChart(this._id);
+        this._group = this._handler.GetGroup(this._id);
+        this._type = this._handler.GetType(this._id);
+        this._parentGroup = this._group.getParent();
+    }
+
+    /**
+     * @summary Removes a chart from the infographic.
+     */
+    Execute()
+    {
+        this._tr.nodes([]);
+        this._main.batchDraw();
+        this._handler.RemoveHandlerElem(this._id);
+    }
+
+    Unexecute()
+    {
+        this._parentGroup.add(this._group);
+        this._handler.AddChart({
+            chart: this._chart,
+            group: this._group,
+            type: this._type
+        });
+        this._chart.CreateChart();
+        this._id = this._handler.GetCurrChartID();
+        this._main.batchDraw();
+    }
 }
 
 export { RemoveChartCommand };
