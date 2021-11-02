e7b04828901203fede386743ef56cc3df973dcbf
Riley Cullen <cullenriley@seattleu.edu>
2021-11-01 18:42:20 -0700

    Export statement for RemoveTextCommand added

--- src/js/Commands/index.js
+++ src/js/Commands/index.js
@@ -3,3 +3,4 @@ export { PositionCommand } from "./CanvasCommands/PositionCommand";
 export { AutoLayerCommand } from './CanvasCommands/AutoLayerCommand';
 export { RemoveGraphicCommand } from './EditorCommands/RemoveGraphicCommand';
 export { RemoveChartCommand } from './EditorCommands/RemoveChartCommand';
+export { RemoveTextCommand } from './EditorCommands/RemoveTextCommand';
