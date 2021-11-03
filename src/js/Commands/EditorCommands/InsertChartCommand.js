b66041e9e9f447309fc85f5bb2699a27d935f1c2
Riley Cullen <cullenriley@seattleu.edu>
2021-11-02 18:17:54 -0700

    Initial commit

---
+++ src/js/Commands/EditorCommands/InsertChartCommand.js
@@ -0,0 +1,21 @@
+import { ACommand } from '../../ACommand';
+
+class InsertChartCommand extends ACommand
+{
+    constructor()
+    {
+        super();
+    }
+
+    Execute()
+    {
+
+    }
+
+    Unexecute()
+    {
+        
+    }
+}
+
+export { InsertChartCommand };
