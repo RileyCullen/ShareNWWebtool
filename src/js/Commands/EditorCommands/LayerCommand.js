f674d32dc8e35260decd2249b551d2b96e720c14
Riley Cullen <cullenriley@seattleu.edu>
2021-11-05 12:47:23 -0700

    constructor, Execute, and Unexecute methods implemented

--- src/js/Commands/EditorCommands/LayerCommand.js
+++ src/js/Commands/EditorCommands/LayerCommand.js
@@ -1,10 +1,20 @@
+import { TextHandler } from "../../Handlers";
 import { ACommand } from "../ACommand";
 
 class LayerCommand extends ACommand 
 {
-    constructor()
+    constructor({handler, id, layerAction})
     {
         super();
+        this._handler = handler;
+        this._id = id;
+        this._layerAction = layerAction;
+
+        this._object = (this._handler instanceof TextHandler) ? 
+            this._handler.GetImage(this._id) : this._handler.GetGroup(this._id);
+        this._initialZIndex = (this._handler instanceof TextHandler) ? 
+            this._handler.GetImage(this._id).zIndex() : 
+            this._handler.GetGroup(this._id).zIndex();
     }
 
     /**
@@ -12,7 +22,7 @@ class LayerCommand extends ACommand
      */
     Execute()
     {
-
+        this._handler.UpdateLayering(this._id, this._layerAction);
     }
 
     /**
@@ -20,7 +30,7 @@ class LayerCommand extends ACommand
      */
     Unexecute()
     {
-
+        this._object.zIndex(this._initialZIndex);
     }
 }
 
