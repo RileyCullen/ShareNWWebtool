e8339376676ed2b2cbed787b4c38be96536d02ee
Riley Cullen <cullenriley@seattleu.edu>
2021-11-04 18:39:50 -0700

    Initial commit

---
+++ src/js/Commands/EditorCommands/InsertIconCommand.js
@@ -0,0 +1,21 @@
+import { ACommand } from '../ACommand';
+
+class InsertIconCommand extends ACommand 
+{
+    constructor()
+    {
+        super();
+    }
+
+    Execute()
+    {
+
+    }
+
+    Unexecute()
+    {
+
+    }
+}
+
+export { InsertIconCommand };
