e5fa7da4b43be383e0fad4c2ecd86ef7b5f24100
Riley Cullen <cullenriley@seattleu.edu>
2021-11-04 19:00:47 -0700

    Implemented constructor, Execute, and Unexecute

--- src/js/Commands/EditorCommands/InsertIconCommand.js
+++ src/js/Commands/EditorCommands/InsertIconCommand.js
@@ -1,20 +1,66 @@
+import { RemoveGraphicCommand } from '..';
 import { ACommand } from '../ACommand';
+import Konva from 'konva';
 
 class InsertIconCommand extends ACommand 
 {
-    constructor()
+    constructor({element, colorScheme, group, handler, transformer, main})
     {
         super();
+        this._element = element;
+        this._colorScheme = colorScheme;
+        this._group = group;
+        this._handler = handler;
+        this._tr = transformer;
+        this._main = main;
+
+        this._id = -1;
+        this._hasCreated = false;
+        this._removeCommand = null;
     }
 
+    /**
+     * @summary Create an icon and add it to the infographic.
+     */
     Execute()
     {
-
+        if (!this._hasCreated) this._CreateIcon();
+        else                   this._removeCommand.Unexecute();
     }
 
+    /**
+     * @summary Remove icon from infographic.
+     */
     Unexecute()
-    {
+    {  
+        this._removeCommand.Execute();
+    }
 
+    _CreateIcon()
+    {
+        // Create Icon
+        let icon = new Konva.Text({
+            text: String.fromCharCode(parseInt(this._element, 16)),
+            fontFamily: '"Font Awesome 5 Free"',
+            fontStyle: '900',
+            fill: this._colorScheme.primary,
+            fontSize: 100,
+        });
+        this._handler.AddGraphic({
+            type: 'icon',
+            graphic: icon,
+            group: this._group,
+        });
+
+        // Update command object
+        this._id = this._handler.GetId();
+        this._hasCreated = true;
+        this._removeCommand = new RemoveGraphicCommand({
+            id: this._id,
+            handler: this._handler,
+            transformer: this._tr,
+            main: this._main,
+        });
     }
 }
 
