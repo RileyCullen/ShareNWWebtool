64038a2fcc8048c18bdbf8fa0579d0b12deb9c56
Riley Cullen <cullenriley@seattleu.edu>
2021-11-04 18:17:18 -0700

    Export statements for InsertText and InsertChart commands added

--- src/js/Commands/index.js
+++ src/js/Commands/index.js
@@ -4,3 +4,5 @@ export { AutoLayerCommand } from './CanvasCommands/AutoLayerCommand';
 export { RemoveGraphicCommand } from './EditorCommands/RemoveGraphicCommand';
 export { RemoveChartCommand } from './EditorCommands/RemoveChartCommand';
 export { RemoveTextCommand } from './EditorCommands/RemoveTextCommand';
+export { InsertTextCommand } from './EditorCommands/InsertTextCommand';
+export { InsertChartCommand } from './EditorCommands/InsertChartCommand';
