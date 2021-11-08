a6332118636dce484a632398069c7fea57d39bb9
Riley Cullen <cullenriley@seattleu.edu>
2021-11-08 11:03:36 -0800

    Initial commit

---
+++ src/js/Commands/EditorCommands/ChartDataCommand.js
@@ -0,0 +1,21 @@
+import { ACommand } from '../ACommand';
+
+class ChartDataCommand extends ACommand 
+{
+    constructor()
+    {
+        super();
+    }
+
+    Execute()
+    {
+
+    }
+
+    Unexecute()
+    {
+
+    }
+}
+
+export { ChartDataCommand };
