b3eedd0a14795a1e5cebd2de0e6c84a0dbd47759
Riley Cullen <cullenriley@seattleu.edu>
2021-11-27 19:46:40 -0800

    Junk removed

--- src/components/InfographicEditor/Editors/Insert/Image.js
+++ src/components/InfographicEditor/Editors/Insert/Image.js
@@ -16,18 +16,15 @@ class Image extends React.Component
         const entry = <div>
             <img src={sessionStorage.getItem(key)}></img>
             <button onClick={() => {sessionStorage.removeItem(key); this._MakeLibrary();}}>Remove</button>
-            <div>Hello!</div>
             </div>
         return entry;
     }
 
     _MakeLibrary(){
         var newLibrary = [];
-        alert(sessionStorage.length)
         for (var i = 0; i < sessionStorage.length; i++)
             newLibrary.push(this.LibraryElement(sessionStorage.key(i)));
         this.setState({library: newLibrary});
-        console.log(this.state.library)
     }
 
     render()
@@ -43,7 +40,6 @@ class Image extends React.Component
                 <div id='upper-image-upload-container'>
                     <input id="upload-image-input" type="file" accept="image/png, image/jpeg, image/jpg" onChange={(event) => {
                         var inputValue = document.getElementById("upload-image-input").value;
-                        alert(event.target.files[0])
                         if (inputValue){
                             var objURL = URL.createObjectURL(event.target.files[0]);
                             this.props.toggleInsert('image', objURL);
@@ -59,7 +55,6 @@ class Image extends React.Component
                                 sessionStorage.setItem(inputValue, objURL);
                                 this._MakeLibrary();
                             }
-                            console.log(sessionStorage)
                         }
                     }}></input>
                 </div>
