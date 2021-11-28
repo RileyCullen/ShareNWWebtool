fd50942dbdf3888ff06b34e12c0162d548cb9fbd
Riley Cullen <cullenriley@seattleu.edu>
2021-11-27 19:44:29 -0800

    filename renamed to objURL. componentDidMount added

--- src/components/InfographicEditor/Editors/Insert/Image.js
+++ src/components/InfographicEditor/Editors/Insert/Image.js
@@ -16,15 +16,18 @@ class Image extends React.Component
         const entry = <div>
             <img src={sessionStorage.getItem(key)}></img>
             <button onClick={() => {sessionStorage.removeItem(key); this._MakeLibrary();}}>Remove</button>
+            <div>Hello!</div>
             </div>
         return entry;
     }
 
     _MakeLibrary(){
         var newLibrary = [];
+        alert(sessionStorage.length)
         for (var i = 0; i < sessionStorage.length; i++)
-            newLibrary.push(<this.LibraryElement key={sessionStorage.key(i)} />);
+            newLibrary.push(this.LibraryElement(sessionStorage.key(i)));
         this.setState({library: newLibrary});
+        console.log(this.state.library)
     }
 
     render()
@@ -40,9 +43,10 @@ class Image extends React.Component
                 <div id='upper-image-upload-container'>
                     <input id="upload-image-input" type="file" accept="image/png, image/jpeg, image/jpg" onChange={(event) => {
                         var inputValue = document.getElementById("upload-image-input").value;
+                        alert(event.target.files[0])
                         if (inputValue){
-                            var filename = URL.createObjectURL(event.target.files[0]);
-                            this.props.toggleInsert('image',filename);
+                            var objURL = URL.createObjectURL(event.target.files[0]);
+                            this.props.toggleInsert('image', objURL);
                             var insert = true;
                             for (var i=0; i<sessionStorage.length; i++){
                                 if (sessionStorage.key(i)===inputValue){
@@ -52,15 +56,21 @@ class Image extends React.Component
                             }
                             if (insert){
                                 console.log("Inserting image in library");
-                                sessionStorage.setItem(inputValue, filename);
+                                sessionStorage.setItem(inputValue, objURL);
                                 this._MakeLibrary();
                             }
+                            console.log(sessionStorage)
                         }
                     }}></input>
                 </div>
                 {uploadMessage}
             </div>
         );
+    }
+
+    componentDidMount()
+    {
+        if (this.state.library.length === 0) this._MakeLibrary();
     }
 }
 
