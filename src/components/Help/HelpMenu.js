68fcc835e5987cfffc9d4dc5712dfb3afb1e2c70
Riley Cullen <cullenriley@seattleu.edu>
2022-03-18 18:21:11 -0700

    Content finalized

--- src/components/Help/HelpMenu.js
+++ src/components/Help/HelpMenu.js
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
+import { HelpElement } from './HelpElement';
+
 import '../../css/React/Help/HelpMenu.css';
 
 function HelpMenu(props)
@@ -6,11 +11,21 @@ function HelpMenu(props)
         <div id="help-menu">
             <div id="help-content-holder">
                 <div id="help-header">
-                    <button onClick={props.onClick}>
-                        Style-less button :P
+                    <h1 className="help-menu-text title">Help Menu</h1>
+                    <button id="help-menu-exit" onClick={props.onClick}>
+                        <FontAwesomeIcon id="help-menu-exit-icon"
+                            className='help-menu-text' 
+                            icon={faTimesCircle}/>
                     </button>
                 </div>
-                <div id="help-main"></div>
+                <div id="help-main">
+                    <HelpElement title="Opening templates">
+                        <p>
+                            To open a template, select the "Use Template" button
+                            underneath the infographic's title.
+                        </p>
+                    </HelpElement>
+                </div>
             </div>
         </div>
     );
