9dc40ef733c06c2e0491be2649404ff07d389e25
Riley Cullen <cullenriley@seattleu.edu>
2022-03-18 17:44:14 -0700

    Initial commit

---
+++ src/components/Help/HelpElement.js
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+import '../../css/React/Help/HelpElement.css';
+
+function HelpElement(props)
+{
+    const [displayContent, setDisplay] = useState(false);
+
+    function toggleContent() {
+        if (!displayContent) setDisplay(true);
+    }
+
+    function removeContent() {
+        setDisplay(false);
+    }
+
+    let content = <h1 className='help-menu-text'>{props.title}</h1>;
+
+    if (displayContent) {
+        content = (
+            <>
+                <div className='help-element-header'>
+                <h1 className='help-menu-text'>{props.title}</h1>
+                <button className='help-element-exit'
+                    onClick={removeContent.bind(this)}>
+                    <FontAwesomeIcon className='help-element-exit-icon' 
+                        icon={faTimesCircle} />
+                </button>
+                </div>
+                {props.children}
+            </>
+        );
+    }
+
+    return (
+        <div className="help-element" onClick={toggleContent.bind(this)}>
+            {content}
+        </div>
+    );
+}
+
+export { HelpElement };
