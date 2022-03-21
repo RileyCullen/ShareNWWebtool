71d40d23ef22785a545bad65151ca4069cd1a5cf
Riley Cullen <cullenriley@seattleu.edu>
2022-03-20 22:56:12 -0700

    Code for checking whether field is in focus or not added

--- src/components/InfographicEditor/Editors/Components/BoundedTextField.js
+++ src/components/InfographicEditor/Editors/Components/BoundedTextField.js
@@ -4,12 +4,17 @@ function BoundedTextField({index, rows, cols, onChange, initialValue,
     isDisabled, lowerBound, upperBound}) 
 {
     const [value, setValue] = useState(initialValue);
+    const [focused, setFocus] = useState(false);
     const prevValue = usePrevious(initialValue);
 
     useEffect(() => {
         if (prevValue !== initialValue) setValue(initialValue);
     }, [initialValue]);
 
+    useEffect(() => {
+        if (value < lowerBound) setValue(lowerBound);
+        if (value > upperBound) setValue(upperBound);
+    }, [focused]);
 
     function handleChange(e) {
         let value = e.target.value;
@@ -21,11 +26,8 @@ function BoundedTextField({index, rows, cols, onChange, initialValue,
         if (value.trim().length === 0 && value !== '') return;
         if (value === '' || isNaN(value)) return;
 
-        // Bounded checks
-        if (value > upperBound) value = upperBound;
-        if (value < lowerBound) value = lowerBound;
-
         setValue(value);
+        if (value < lowerBound || value > upperBound) return;
         onChange(value, index);
     }
 
@@ -38,6 +40,8 @@ function BoundedTextField({index, rows, cols, onChange, initialValue,
                 onChange={handleChange}
                 value={value}
                 disabled={isDisabled}
+                onFocus={() => { setFocus(true); }}
+                onBlur={() => { setFocus(false); }}
             />
         </div>
     )
