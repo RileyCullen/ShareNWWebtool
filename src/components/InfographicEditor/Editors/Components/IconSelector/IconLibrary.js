b1f8cbd35fb78b28134d2c77217a4c8b697c3dd0
Riley Cullen <cullenriley@seattleu.edu>
2022-05-19 18:50:18 -0700

    useOutsideClick hook added

--- src/components/InfographicEditor/Editors/Components/IconSelector/IconLibrary.js
+++ src/components/InfographicEditor/Editors/Components/IconSelector/IconLibrary.js
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import useOutsideClick from '../../../../Hooks/HandleOutsideClick';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import ICON_LIST from '../../../IconList';
@@ -9,8 +11,12 @@ import '../../../../../css/React/Editors/IconLibrary.css';
  * @param {Object} props closeLibrary: function, changeIcon: function
  */
 const IconLibrary = (props) => {
+    const ref = useRef(null);
+    
+    useOutsideClick(ref, props.closeLibrary);
+
     return (
-        <div className='icon-library-container'>
+        <div ref={ref} className='icon-library-container'>
             <div className='icon-library-header'>
                 <button className='icon-library-close' onClick={props.closeLibrary}>
                     <FontAwesomeIcon className='close-icon' icon={faTimesCircle} />
